4d62aacaa275d71e4c349816d68e76213fc10489
Christ Kankolongo <christ.kankolongo@adevinta.com>
2023-12-14 10:10:33 +0100

    Install EsLint in the project

---
+++ .eslintrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  env: {
+    node: true
+  },
+  parser: "@typescript-eslint/parser",
+  root: true,
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"]
+}
